fce7713cb94c9fb1f1e21829eab6f07d542b4e43
psai0011 <85570618+psai0011@users.noreply.github.com>
2025-03-24 08:57:51 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,48 +1,47 @@
 from crewai import Agent
-from langchain_groq import ChatGroq
-import os
+from tools import tool
 from dotenv import load_dotenv
-from crewai_tools import YoutubeChannelSearchTool
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 # Load environment variables
 load_dotenv()
 
-# Initialize Groq LLM
-llm = ChatGroq(
-    model_name="llama3-70b-8192",  
-    temperature=0.7,
-    groq_api_key=os.environ["GROQ_API_KEY"]
+# Initialize the Gemini model
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    google_api_key=os.getenv("GOOGLE_AOP_KEY"),
+    verbose=True,
+    temperature=0.5
 )
 
-# Define YouTube tool since it's imported but not defined
-yt_tool = YoutubeChannelSearchTool()
-
-# Create a senior blog content researcher
-blog_researcher = Agent(
-    role="Blog Researcher from Youtube Videos",
-    goal="Get the relevant video content for the topic {topic} from yt channel",
+# Creating the senior research agent with memory and verbose mode
+news_researcher = Agent(
+    role="Senior Researcher",
+    goal="Uncover groundbreaking technologies in {topic}",
     verbose=True,
-    memory=True,
     backstory=(
-        "Expert in understanding Videos in AI Data Science, Machine Learning and GEN AI and providing suggestion "
+        "Driven by curiosity, you're at the forefront of "
+        "innovation, eager to explore and share knowledge that could change "
+        "the world."
     ),
-    tools=[yt_tool],
+    tools=[tool],
     llm=llm,
     allow_delegation=True
 )
 
-# Create a senior writer agent with YT tool
-blog_writer = Agent(
-    role="Blog Writer",
-    goal="Narrate compelling tech stories about the video {topic} from yt channel",
+# Creating the writer agent with custom tools and responsible for writing news blogs
+news_writer = Agent(
+    role="Writer",
+    goal="Narrate compelling tech stories about {topic}",
     verbose=True,
-    memory=True,
     backstory=(
         "With a flair for simplifying complex topics, you craft "
         "engaging narratives that captivate and educate, bringing new "
         "discoveries to light in an accessible manner."
     ),
-    tools=[yt_tool],
+    tools=[tool],
     llm=llm,
     allow_delegation=False
 )
